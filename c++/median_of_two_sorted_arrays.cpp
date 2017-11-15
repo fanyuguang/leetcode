54889b5cac2480ebcf303c8fc1c8d2a76ed40e76
fanyuguang [范玉广] <fanyuguang@xiaomi.com>
2017-11-15 22:22:48 +0800

    update

--- c++/median_of_two_sorted_arrays.cpp
+++ c++/median_of_two_sorted_arrays.cpp
@@ -6,12 +6,17 @@ class Solution {
 public:
 	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
 	}
+
+private:
+  double findKth(vector<int>& nums1, vector<int>& nums2, int k) {
+  }
 };
 
 int main() {
 	vector<int> nums1 = {1, 3};
 	vector<int> nums2 = {2};
 	Solution s;
-	cout << s.findMedianSortedArrays(nums1, nums2) << endl;
+  double result = s.findMedianSortedArrays(nums1, nums2);
+	cout << result << endl;
 	return 0;
 }
